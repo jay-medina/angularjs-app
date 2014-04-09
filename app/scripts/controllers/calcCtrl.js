a72cae0445be7a0ae94ae1c504c843ccfbbd50c7
=Jose Medina <josemedina921@gmail.com>
2014-04-09 15:56:35 -0400

    Update to add secondValue functionality

--- app/scripts/controllers/calcCtrl.js
+++ app/scripts/controllers/calcCtrl.js
@@ -2,49 +2,81 @@
 
 angular.module('angularDemoApps')
 .controller('CalcCtrl', ['$scope', function($scope){
-		$scope.finalVal = 0;
-		$scope.val = '0';
+		$scope.values = {displayValue: '0', currentValue: 0, total: 0};
 		$scope.equation = '';
+		$scope.sign = '';
 
 		$scope.backTrack = function(){
-			if($scope.val !== '0' && $scope.equation === ''){
-				$scope.val = $scope.val.substring(0, $scope.val.length-1);
-				if($scope.val === ''){
+			if($scope.values.currentValue !== 0){
+				var displayValue = $scope.values.displayValue;
+				$scope.values.displayValue = displayValue.slice(0, displayValue.length-1);
+				if($scope.values.displayValue === ''){
 					$scope.clearValue();
 				}
 			}
 		};
 
 		$scope.clearValue = function(){
-			$scope.val = '0';
+			$scope.values.displayValue = '0';
+			$scope.values.currentValue = 0;
+			$scope.values.total = 0;
 			setEquation('reset');
 		};
 
 		$scope.setValue = function(val){
-			if($scope.val === '0'){
-				if(val !== 0){
-					$scope.val = val;
-				}
+			if($scope.sign === ''){
+				set.InitialValue(val);
 			}
 			else{
-				$scope.val += '' + val;
+				set.SecondValue(val);
 			}
 		};
 
 		$scope.setOperation = function(op){
 			if('+' === op || '-' === op || '*' === op || '/' === op){
+
 				setEquation(op);
+				$scope.values.total = $scope.values.currentValue;
+				$scope.values.currentValue = 0;
+
+
 			}
 		};
 
 		var setEquation = function(eqSign){
 			if(eqSign === 'reset'){
-				$scope.equation = '';
+				$scope.equation = $scope.sign = '';
 			}
 			else{
-				$scope.equation = $scope.val + '  ' + eqSign;
+				$scope.equation = $scope.values.displayValue + '  ' + eqSign;
+				$scope.sign = eqSign;
 			}
 			
 		};
 
+		var set = {
+			InitialValue: function(val){
+				if($scope.values.displayValue === '0'){
+				  $scope.values.displayValue = '' + val;
+				}
+				else{
+				  $scope.values.displayValue += val;
+				}
+
+				$scope.values.currentValue = Number($scope.values.displayValue);
+			},
+			SecondValue: function(val){
+				if($scope.values.currentValue === 0){
+					$scope.values.displayValue = '' + val;
+				}
+				else{
+					$scope.values.displayValue += val;
+				}
+
+				$scope.values.currentValue = Number($scope.values.displayValue);
+			}
+		};
+
   }]);
+
+
